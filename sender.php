3b9aefaff0d56100e20402a0390bb20d0cd0cc89
Regin2025-lang <kamboja2025@gmail.com>
2026-06-11 15:24:29 +0700

    Update sender.php

--- sender.php
+++ sender.php
@@ -5,22 +5,49 @@ $config = json_decode(
     true
 );
 
+
+$response = file_get_contents(
+    $config['api_url']
+);
+
+
+if($response === false){
+
+    die("Tidak bisa ambil API InfinityFree");
+}
+
+
 $data = json_decode(
-    file_get_contents($config['api_url']),
+    $response,
     true
 );
 
+
+if(!isset($data['message'])){
+
+    echo "Response InfinityFree:";
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+
+    exit;
+}
+
+
 $message = $data['message'];
+
 
 $url =
 "https://api.telegram.org/bot".
 $config['telegram_token'].
 "/sendMessage";
 
+
 $post = [
-    'chat_id' => $config['chat_id'],
-    'text' => $message
+    "chat_id"=>$config['chat_id'],
+    "text"=>$message
 ];
+
 
 $ch = curl_init();
 
@@ -29,8 +56,18 @@ curl_setopt($ch,CURLOPT_POST,true);
 curl_setopt($ch,CURLOPT_POSTFIELDS,$post);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 
+
 $result = curl_exec($ch);
 
-curl_close($ch);
 
-echo $result;
+if(curl_errno($ch)){
+
+    echo curl_error($ch);
+
+}else{
+
+    echo $result;
+}
+
+
+curl_close($ch);
