97f12415d2064be575f5c916cc8619002e096047
Regin2025-lang <kamboja2025@gmail.com>
2026-06-11 16:35:06 +0700

    Update sender.php

--- sender.php
+++ sender.php
@@ -6,19 +6,58 @@ $config = json_decode(
 );
 
 
+// ambil langsung config dari InfinityFree
+
+$remote = file_get_contents(
+    "https://urakurak.site.je/config.json"
+);
+
+
+if($remote === false){
+
+    die("Gagal ambil config InfinityFree");
+
+}
+
+
+$data = json_decode($remote, true);
+
+
+if(!isset($data['message'])){
+
+    echo "ERROR RESPONSE:";
+    echo "<pre>";
+    print_r($remote);
+    echo "</pre>";
+    exit;
+
+}
+
+
+$message = $data['message'];
+
+
+// kirim ke Telegram
+
+$tg = "https://api.telegram.org/bot".
+$config['telegram_token'].
+"/sendMessage";
+
+
+$post = [
+    "chat_id"=>$config['chat_id'],
+    "text"=>$message
+];
+
+
 $ch = curl_init();
 
-curl_setopt($ch,CURLOPT_URL,$config['api_url']);
+curl_setopt($ch,CURLOPT_URL,$tg);
+curl_setopt($ch,CURLOPT_POST,true);
+curl_setopt($ch,CURLOPT_POSTFIELDS,$post);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 
-curl_setopt($ch,CURLOPT_HEADER,true);
 
-$response = curl_exec($ch);
-
-
-echo "<pre>";
-echo $response;
-echo "</pre>";
-
+echo curl_exec($ch);
 
 curl_close($ch);
