edac95c8c2cbe9d8dfbc2273aa779fcb3ff37322
Regin2025-lang <kamboja2025@gmail.com>
2026-06-11 16:26:08 +0700

    Update sender.php

--- sender.php
+++ sender.php
@@ -11,50 +11,14 @@ $ch = curl_init();
 curl_setopt($ch,CURLOPT_URL,$config['api_url']);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 
+curl_setopt($ch,CURLOPT_HEADER,true);
+
 $response = curl_exec($ch);
 
-curl_close($ch);
 
+echo "<pre>";
+echo $response;
+echo "</pre>";
 
-$data = json_decode(
-    $response,
-    true
-);
-
-
-if(!isset($data['message'])){
-
-    echo "API gagal:";
-    echo "<pre>";
-    print_r($response);
-    echo "</pre>";
-    exit;
-}
-
-
-$message=$data['message'];
-
-
-$url=
-"https://api.telegram.org/bot".
-$config['telegram_token'].
-"/sendMessage";
-
-
-$post=[
-"chat_id"=>$config['chat_id'],
-"text"=>$message
-];
-
-
-$ch=curl_init();
-
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_POST,true);
-curl_setopt($ch,CURLOPT_POSTFIELDS,$post);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-
-
-echo curl_exec($ch);
 
 curl_close($ch);
